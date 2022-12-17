b2b78ab92c027ff16033e0ac3e6b0a09bd253950
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-12-17 10:52:40 -0500

    server.js

--- server/server.js
+++ server/server.js
@@ -23,15 +23,15 @@ const server = new ApolloServer({
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
- 
+
 // if we're in production, serve client/build as static assets
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+app.use(express.static(path.join(__dirname, "../client/build")));
+// }
 
-app.get("/",(req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // app.use(routes);
